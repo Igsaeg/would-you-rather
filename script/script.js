772cebfe3106460d1097e281f819a24c003967dd
Igsaeg <60771249+Igsaeg@users.noreply.github.com>
2023-11-13 19:20:17 +0800

    add jokes

--- script/script.js
+++ script/script.js
@@ -1,35 +1,29 @@
 const pParent = document.getElementsByClassName('item');
 const buttonParent = document.getElementsByClassName('choices');
-let i = -1;
 let choice = 0;
-function a() {
+function a(i) {
     choice = 0;
-    update();
+    update(i);
     let a = buttonParent[i].querySelector('button:nth-child(1)')
     a.id = `picked`;
     let b = buttonParent[i].querySelector('button:nth-child(2)')
     b.id = `unpicked`;
 }
-function b() {
+function b(i) {
     choice = 1;
-    update();
+    update(i);
     let a = buttonParent[i].querySelector('button:nth-child(2)')
     a.id = `picked`;
     let b = buttonParent[i].querySelector('button:nth-child(1)')
     b.id = `unpicked`;
 }
-function update() {
-    i++;
-    let firstParagraph = pParent[i].querySelector('p');
-    firstParagraph.innerText = `You have chosen ${choices[i][choice]}`;
-    let a = buttonParent[i+1].querySelector('button:nth-child(1)');
+function update(i) {
+    let question = pParent[i].querySelector('p:nth-child(1)');
+    question.innerText = `You have chosen ${choices[i][choice]}`;
+    let a = buttonParent[i].querySelector('button:nth-child(1)');
     a.removeAttribute('id');
-    let b = buttonParent[i+1].querySelector('button:nth-child(2)');
+    let b = buttonParent[i].querySelector('button:nth-child(2)');
     b.removeAttribute('id');
-}
-for (let x = 1; x < buttonParent.length; x++) {
-    let a = buttonParent[x].querySelector('button:nth-child(1)');
-    a.id = `disabled`;
-    let b = buttonParent[x].querySelector('button:nth-child(2)');
-    b.id = `disabled`;
+    let pun = pParent[i].querySelector('p:nth-child(3)');
+    pun.style.display = `block`;
 }
